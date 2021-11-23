8870df88e165db923405ce84d0fed7631a61bc88
Jincai Yang <jincai.yang@foxmail.com>
2021-11-23 20:30:12 +0800

    add option to download vscode server (linux).

--- cndlvsc.py
+++ cndlvsc.py
@@ -17,20 +17,23 @@
 
 更多介绍： https://note.qidong.name/2020/05/dl-vscode-cn/
 """
+import re
+import tarfile
 from argparse import ArgumentParser
-from os.path import split
+from pathlib import Path
 from urllib.parse import urlparse, urlunparse
 
 import requests
 from tqdm import tqdm
 
-CN_DOMAIN = 'vscode.cdn.azure.cn'
+CN_DOMAIN = "vscode.cdn.azure.cn"
 TYPE2URL = {
-    'win32': 'https://update.code.visualstudio.com/latest/win32/stable',
-    'win64': 'https://update.code.visualstudio.com/latest/win32-x64/stable',
-    'deb': 'https://update.code.visualstudio.com/latest/linux-deb-x64/stable',
-    'rpm': 'https://update.code.visualstudio.com/latest/linux-rpm-x64/stable',
-    'mac': 'https://update.code.visualstudio.com/latest/darwin/stable',
+    "win32": "https://update.code.visualstudio.com/latest/win32/stable",
+    "win64": "https://update.code.visualstudio.com/latest/win32-x64/stable",
+    "deb": "https://update.code.visualstudio.com/latest/linux-deb-x64/stable",
+    "rpm": "https://update.code.visualstudio.com/latest/linux-rpm-x64/stable",
+    "mac": "https://update.code.visualstudio.com/latest/darwin/stable",
+    "server": "https://update.code.visualstudio.com/latest/linux-rpm-x64/stable",
 }
 
 
@@ -42,24 +45,49 @@ def main():
     resp.raise_for_status()
     dl_url, fname = replace_domain(resp.next.url, CN_DOMAIN)
 
+    if args.type == "server":
+        # before: https://vscode.cdn.azure.cn/stable/ccbaa2d27e38e5afa3e5c21c1c7bef4657064247/code-1.62.3-1637137194.el7.x86_64.rpm
+        # after: https://vscode.cdn.azure.cn/stable/ccbaa2d27e38e5afa3e5c21c1c7bef4657064247/vscode-server-linux-x64.tar.gz
+        fname = "vscode-server-linux-x64.tar.gz"
+        parts = dl_url.split("/")
+        parts[-1] = fname
+        dl_url = "/".join(parts)
+
+        commit = parts[-2]
+        server_dir = Path.home() / ".vscode-server/bin" / commit
+
     if args.print_only:
         print(dl_url)
     else:
-        download(dl_url, fname)
+        if args.type == "server":
+            if server_dir.exists():
+                raise SystemExit(
+                    f"\nError: Latest vscode-server(linux) dir exists:\n{server_dir}\n"
+                    f"Please delete it if you want to reinstall.\n"
+                )
+            else:
+                download(dl_url, fname)
+                server_dir.parent.mkdir(parents=True, exist_ok=True)
+                with tarfile.open(fname, "r:gz") as tar:
+                    tar.extractall(path="./")
+                    Path("./vscode-server-linux-x64").rename(server_dir)
+                    print(f"extract vscode-server to {server_dir}")
+        else:
+            download(dl_url, fname)
 
 
 def _parse_args():
     parser = ArgumentParser()
     parser.add_argument(
-        'type',
+        "type",
         choices=TYPE2URL,
-        help='The download type',
+        help="The download type",
     )
     parser.add_argument(
-        '-p',
-        '--print-only',
-        action='store_true',
-        help='Print url only',
+        "-p",
+        "--print-only",
+        action="store_true",
+        help="Print url only",
     )
     return parser.parse_args()
 
@@ -68,24 +96,25 @@ def replace_domain(url, domain):
     result = urlparse(url)
     lst = list(result)
     lst[1] = domain
-    _, fname = split(result.path)
+    fname = Path(result.path).name
     return urlunparse(lst), fname
 
 
 def download(url, fname):
+    print(f"Download latest vscode from {url}")
     resp = requests.get(url, stream=True)
-    total = int(resp.headers.get('content-length', 0))
-    with open(fname, 'wb') as file, tqdm(
-            desc=fname,
-            total=total,
-            unit='iB',
-            unit_scale=True,
-            unit_divisor=1024,
+    total = int(resp.headers.get("content-length", 0))
+    with open(fname, "wb") as file, tqdm(
+        desc=fname,
+        total=total,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
     ) as bar:
         for data in resp.iter_content(chunk_size=1024):
             size = file.write(data)
             bar.update(size)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
